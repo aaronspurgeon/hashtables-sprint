50a3264c27f30aa1e03cde0f0f493b825f9a7119
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-14 20:49:01 -0400

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    Ticket = {}
+    route = []
+
+    for i in tickets:
+        Ticket[i.source] = i.destination
+
+    for j in range(length):
+        if not route:
+            route.append(Ticket['NONE'])
+        else:
+            route.append(Ticket[route[-1]])
 
     return route
