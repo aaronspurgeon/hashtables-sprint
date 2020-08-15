63d9d5fb373b4ec1647c3bb6c6b17f15f5ae81e6
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-14 20:51:18 -0400

    ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
+    result = []
+
+    for i in a:
+        if i != 0:
+            hash_table[i] = 1
+            if -i in hash_table:
+                result.append(abs(i))
 
     return result
 
