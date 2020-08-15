631292dde7378b2006370d19ee91f5ddacc1185e
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-14 20:45:16 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {}
 
-    return None
+    for i in range(length):
+        item = weights[i]
+        target = limit - item
+
+        if target not in hash_table:
+            hash_table[item] = i
+        else:
+            target_index = hash_table[target]
+            return (i, target_index)
